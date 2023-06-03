0b28487fc6cbca25cd63e449eca3b85eeb880868
Coding 'Yena <135306622+CodingYena@users.noreply.github.com>
2023-06-03 00:21:20 -0400

    Update 10to11.ps1

--- Windows-10-to-11-Upgrade-Script/10to11.ps1
+++ Windows-10-to-11-Upgrade-Script/10to11.ps1
@@ -1,17 +1,50 @@
-# Download the Windows 11 ISO
-$isoUrl = "https://www.itechtics.com/?dl_id=168"
-$isoPath = "$env:TEMP\Windows11.iso"
-Invoke-WebRequest -Uri $isoUrl -OutFile $isoPath
+# Define the log file path
+$logFilePath = "$env:TEMP\upgrade_log.txt"
 
-# Mount the Windows 11 ISO
-$mountPath = "D:"
-Mount-DiskImage -ImagePath $isoPath -PassThru | Get-Volume | Get-Partition | Get-Disk | Set-Disk -IsOffline $false
-Mount-DiskImage -ImagePath $isoPath -PassThru | Get-Volume | Get-Partition | Get-Disk | Set-Disk -IsReadOnly $false
+# Function to log error messages
+function Log-Error {
+    param($message)
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $errorLog = "[$timestamp] ERROR: $message"
+    $errorLog | Out-File -FilePath $logFilePath -Append
+}
 
-# Upgrade to Windows 11
-$setupPath = "$mountPath:\setup.exe"
-Start-Process -FilePath $setupPath -ArgumentList "/auto upgrade /DynamicUpdate disable /QuietInstall /ShowOOBE none" -Wait
+try {
+    # Download the Windows 11 ISO
+    $isoUrl = "https://www.itechtics.com/?dl_id=168"
+    $isoPath = "$env:TEMP\Windows11.iso"
+    Invoke-WebRequest -Uri $isoUrl -OutFile $isoPath
 
-# Clean up
-Dismount-DiskImage -ImagePath $isoPath
-Remove-Item -Path $isoPath
+    # Mount the Windows 11 ISO
+    $mountPath = "D:"
+    $diskImage = Mount-DiskImage -ImagePath $isoPath -PassThru
+    $diskImage | Get-Volume | Get-Partition | Get-Disk | Set-Disk -IsOffline $false
+    $diskImage | Get-Volume | Get-Partition | Get-Disk | Set-Disk -IsReadOnly $false
+
+    # Upgrade to Windows 11
+    $setupPath = Join-Path -Path $mountPath -ChildPath "setup.exe"
+    Start-Process -FilePath $setupPath -ArgumentList "/auto upgrade /DynamicUpdate disable /QuietInstall /ShowOOBE none" -Wait
+}
+catch {
+    $errorMessage = $_.Exception.Message
+    Log-Error -message $errorMessage
+}
+
+finally {
+    # Clean up
+    try {
+        Dismount-DiskImage -ImagePath $isoPath -ErrorAction Stop
+    }
+    catch {
+        $errorMessage = $_.Exception.Message
+        Log-Error -message $errorMessage
+    }
+
+    try {
+        Remove-Item -Path $isoPath -ErrorAction Stop
+    }
+    catch {
+        $errorMessage = $_.Exception.Message
+        Log-Error -message $errorMessage
+    }
+}
